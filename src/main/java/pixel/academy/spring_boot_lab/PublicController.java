bc04ff4a668e3728b5f89ec279d8fea2523bd8d7
Sevastian Vascan <svascan@novoinvestment.md>
2025-06-03 15:20:41 +0300

    chore: Add 2 @Beans.

--- src/main/java/pixel/academy/spring_boot_lab/PublicController.java
+++ src/main/java/pixel/academy/spring_boot_lab/PublicController.java
@@ -1,7 +1,11 @@
 package pixel.academy.spring_boot_lab;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +18,16 @@ public class PublicController {
         return "Returneaza un text la alegere";
     }
 
- /*   @Bean
+
+    // Try to exclude "/public" from Spring Boot Security
+    @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web -> web.ignoring().requestMatchers("/public"));
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().());
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
         return http.build();
-    }*/
+    }
 }
