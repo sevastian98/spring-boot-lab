9748b2110b56fcf18ef6f7f93b575a734bf2bb2a
Sevastian <sevastian.vascan7@gmail.com>
2025-06-03 15:36:28 +0300

    chore: Add @Bean in PublicControoler  which exclude '/public' endpoint from Spring Boot Security.

--- src/main/java/pixel/academy/spring_boot_lab/PublicController.java
+++ src/main/java/pixel/academy/spring_boot_lab/PublicController.java
@@ -2,6 +2,7 @@ package pixel.academy.spring_boot_lab;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -15,19 +16,13 @@ public class PublicController {
 
     @GetMapping("/public")
     public String publicMethod() {
-        return "Returneaza un text la alegere";
+        return "Acest text se afiseaza indiferent ca SpringBoot security este activ (pentru el nu este necesar de log si pass)";
     }
 
 
-    // Try to exclude "/public" from Spring Boot Security
+    // Exclude endpoint "/public" from Spring Boot Security
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web -> web.ignoring().requestMatchers("/public"));
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
-        return http.build();
     }
 }
